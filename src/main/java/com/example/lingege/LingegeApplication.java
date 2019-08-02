f24da89495a9985fb7fe440d81cd302afcb1b19d
Linruixiang <Linruixiang@npnet.com>
2019-08-02 15:56:48 +0800

    Initial commit

---
+++ src/main/java/com/example/lingege/LingegeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lingege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingegeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingegeApplication.class, args);
+    }
+
+}
